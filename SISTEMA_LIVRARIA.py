305057658548d9c32262cacc4f39b3e11e5fe446
Peedron <2025023475@aluno.restinga.ifrs.edu.br>
2026-03-26 18:16:18 -0300

    Primeiro commit: nova lógica de código

--- SISTEMA_LIVRARIA.py
+++ SISTEMA_LIVRARIA.py
@@ -1,193 +1,209 @@
-try:
-    # função para separação de informações
-    def linha():
-        print("-="*20)
+# SISTEMA LIVRARIA
 
-    # criando classe livraria
-    class Livraria:
-        # definição dos atributos da classe
-        def __init__(self, 
-                    titulo,
-                    codigo,
-                    editora,
-                    area,
-                    ano,
-                    valor,
-                    quantidade_estoque):
-            self.titulo = titulo
-            self.codigo = codigo
-            self.editora = editora
-            self.area = area
-            self.ano = ano
-            self.valor = valor
-            self.quantidade_estoque = quantidade_estoque # fazer valor de estoque
-            self.valor_estoque = self.quantidade_estoque * self.valor
+# função para separar informações
+def linha():
+    print('-='*20)
 
-    global lista_livros # definindo lista como variável global
-    lista_livros = list() # criando lista que irá receber os livros
+# criando classe 
+class Livro:
+    def __init__(self, titulo,codigo,editora,area,ano,valor,quantidade_estoque):
+        self.titulo = titulo
+        self.codigo = codigo
+        self.editora = editora
+        self.area = area
+        self.ano = ano
+        self.valor = valor
+        self.quantidade_estoque = quantidade_estoque
 
-    # função para o usuário cadastrar livros
-    def cadastro_livros():
-        lista_livros.append(Livraria(
-            titulo=input("Título do livro: ").title(),
-            codigo= input("Código: "),
-            editora= input("Editora: ").title(),
-            area= input("Área: ").title(),
-            ano= input("Ano: "),
-            valor= float(input("Valor: ")),
-            quantidade_estoque= int(input("Quantidade: "))))
+global lista_livros
+lista_livros=list()
 
-    # função com as informações dos livros cadastrados
-    def acervo():
-        # mensagem para o usuário
-        linha()
-        print("Esses são os livros cadastrados:\n")
-        # laço de repetição para mostrar as informações de cada livro
-        for livro in lista_livros:
-            print(f">>>>>Cod#{livro.codigo}")
+# função calcula o valor do estoque de livros
+def calcula_valor_estoque(a, b):
+    valor = a*b
+    
+    return (valor)
+
+# função de cadastro de livros
+def cadastro_livro():
+    linha()
+    lista_livros.append(Livro(titulo=input("Título do livro: ").title(),
+                            codigo=input("Código: "),
+                            editora=input("Editora: ").title(),
+                            area=input("Área: ").title(),
+                            ano=input("Ano: "),
+                            valor=float(input("Valor: ")),
+                            quantidade_estoque=int(input("Número de unidades: "))))
+    linha()
+
+# função que mostra os livros cadastrados
+def info_livros():
+    linha()
+    print(" ---- Livros cadastrados ---- ")
+    for livro in lista_livros:
+        print(f"\nCod#{livro.codigo}")
+        print(f"Título/Editora: {livro.titulo}/{livro.editora}")
+        print(f"Categoria: {livro.area}")
+        print(f"Ano: {livro.ano}")
+        print(f"Valor: R$ {livro.valor:.2f}")
+        print(f"Estoque: {livro.quantidade_estoque} unidades")
+        
+        # chamando função para calcular o valor do Estoque
+        valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+        print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+        
+    linha()
+
+# função busca o livro pelo nome:
+def buscar_nome():
+    linha()
+    
+    nome = input("Título do livro: ").title()
+    
+    registro = False
+    
+    for livro in lista_livros:
+        if nome in livro.titulo:
+            registro = True
+            print("\nLivro encontrado:")
+            print(f"\nCod#{livro.codigo}")
             print(f"Título/Editora: {livro.titulo}/{livro.editora}")
             print(f"Categoria: {livro.area}")
             print(f"Ano: {livro.ano}")
             print(f"Valor: R$ {livro.valor:.2f}")
             print(f"Estoque: {livro.quantidade_estoque} unidades")
-            print(f"Valor total em estoque: R$ {livro.valor_estoque:.2f}")
-
-    # função para encontrar livros por nome
-    def buscar_titulo(): 
-        # variável recebe o nome do livro e já formata o texto
-        nome = input("Digite o título do livro que deseja: ").title()
-
-        contador = 0
-        linha()
-        for livro in lista_livros:
-            if nome not in livro.titulo:
-                contador += 1
-                print("Livro encontrado! Essas são as informações:\n")
-                print(f">>>>>Cod#{livro.codigo}")
-                print(f"Título/Editora: {livro.titulo}/{livro.editora}")
-                print(f"Categoria: {livro.area}")
-                print(f"Ano: {livro.ano}")
-                print(f"Valor: R$ {livro.valor:.2f}")
-                print(f"Estoque: {livro.quantidade_estoque} unidades")
-                print(f"Valor total em estoque: R$ {livro.valor_estoque:.2f}")
-        
-        if contador == 0:
-            print("Este livro não se encontra no acervo!")
-
-    # função para buscar livros por suas categorias/áreas
-    def buscar_categoria():
-        categoria = input("Qual categoria de livros que deseja ver? ").title()
-
-        contador = 0
-        linha()
-        for livro in lista_livros:
-            if categoria in livro.area:
-                contador += 1
-                print(f">>>>>Cod#{livro.codigo}")
-                print(f"Título/Editora: {livro.titulo}/{livro.editora}")
-                print(f"Categoria: {livro.area}")
-                print(f"Ano: {livro.ano}")
-                print(f"Valor: R$ {livro.valor:.2f}")
-                print(f"Estoque: {livro.quantidade_estoque} unidades")
-                print(f"Valor total em estoque: R$ {livro.valor_estoque:.2f}")
-        
-        if contador == 0:
-            print("Não há livros com essa categoria!")
-
-    # função de comparação de preços
-    def preco_menor():
-        preco = float(input("Deseja ver livros abaixo de qual valor? "))
-
-        # contador ajuda a saber se existe pelo menos um livro no até o valor indicado
-        contador = 0
-        linha()
-        print(f"Livros até R$ {preco:.2f}:\n")
-        for livro in lista_livros:
-            if livro.valor < preco:
-                contador += 1
-                print(f">>>>>Cod#{livro.codigo}")
-                print(f"Título/Editora: {livro.titulo}/{livro.editora}")
-                print(f"Categoria: {livro.area}")
-                print(f"Ano: {livro.ano}")
-                print(f"Valor: R$ {livro.valor:.2f}")
-                print(f"Estoque: {livro.quantidade_estoque} unidades")
-                print(f"Valor total em estoque: R$ {livro.valor_estoque:.2f}")
-
-        if contador == 0:
-            linha()
-            print("Não há livros neste valor!")
-                
-    # função que busca livros com valor de estoque maior
-    def valor_estoque():
-        valor = int(input("Digite um valor de estoque: "))
-
-        linha()
-        for livro in lista_livros:
-            if livro.valor_estoque > valor:
-                # informações do livro
-                a = 1
-
-    # variáveis fixas para que a livraria já tenha livros cadastrados
-    livro1 = Livraria(titulo="O Avesso Da Pele",
-                    codigo="082",
-                    editora="Companhia Das Letras",
-                    area="Romance",
-                    ano="2021",
-                    valor=47.60,
-                    quantidade_estoque=12)
-
-    # livros são guardados na lista
-    lista_livros.append(livro1)
-
-    # função menu para a navegação pelo sistema
-    def menu():
-        print("Bem vindo ao sistema livraria!")
-        print("Aqui estão as opções do que fazer:\n")
-
-        print("1-Cadastrar novo livro")
-        print("2-Listar livros")
-        print("3-Buscar livros por nome")
-        print("4-Buscar livros por categoria")
-        print("5-Buscar livros por preço")
-        print("6-Busca por quantidade em estoque")
-        print("7-Valor total no estoque")
-        print("0-Encerrar atividades\n")
-
-        repetir = True
-
-        while repetir:
-            escolha = int(input("Sua escolha: "))
-
-            if escolha == 1:
-                cadastro_livros()
-            elif escolha == 2:
-                acervo()
-            elif escolha == 3:
-                buscar_titulo()
-            elif escolha == 4:
-                buscar_categoria()
-            elif escolha == 5:
-                preco_menor()
-            elif escolha == 6:
-                valor_estoque()
-            elif escolha == 0:
-                sair()
-
             
-            resposta = input("Deseja continuar? ").lower()
+            # chamando função para calcular o valor do Estoque
+            valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+            print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+    
+    if not registro:
+        print("Livro não encontrado!")
+    
+    linha()
+    
+# função apresenta livros de uma categoria específica
+def categoria():
+    linha()
+    # recebendo a categoria
+    categoria = input("Categoria de livros: ")
+    
+    registro = False
+    for livro in lista_livros:
+        if categoria in livro.area:
+            registro = True
+            print(f"\nCod#{livro.codigo}")
+            print(f"Título/Editora: {livro.titulo}/{livro.editora}")
+            print(f"Categoria: {livro.area}")
+            print(f"Ano: {livro.ano}")
+            print(f"Valor: R$ {livro.valor:.2f}")
+            print(f"Estoque: {livro.quantidade_estoque} unidades")
+            
+            # chamando função para calcular o valor do Estoque
+            valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+            print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+    
+    if not registro:
+        print("Não há livros com está categoria!")
+    linha()
 
-            if resposta == "nao":
-                print("\nFim do programa! Volte sempre")
-                repetir = False
-            else:
-                continue
+# função demonstra livros que tem preço menor do que um valor
+def limite_valor():
+    linha()
+    preco = float(input("Defina um limte de preço: "))
+    registro = False
     
-    # função encerra o programa caso o usuário digite '0'
-    def sair():
-        linha()
-        print("Você escolheu encerrar o programa!\n")
-        raise(EOFError)
+    for livro in lista_livros:
+        if livro.valor < preco:
+            registro = True
+            print("\nLivro encontrado:")
+            print(f"\nCod#{livro.codigo}")
+            print(f"Título/Editora: {livro.titulo}/{livro.editora}")
+            print(f"Categoria: {livro.area}")
+            print(f"Ano: {livro.ano}")
+            print(f"Valor: R$ {livro.valor:.2f}")
+            print(f"Estoque: {livro.quantidade_estoque} unidades")
+            
+            # chamando função para calcular o valor do Estoque
+            valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+            print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
     
-    menu()
-except EOFError:
-    print("Fim do programa!")
+    if not registro:
+        print("Não há livros nessa faixa de preço!")
+    linha()
+
+# função busca livros com valor de estoque maior que o indicado
+def compara_valor_estoque():
+    linha()
+    valor_indicado = float(input("Digite o valor do estoque: "))
+    
+    for livro in lista_livros:
+        # chamando função para calcular o valor do Estoque
+        valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+        
+        if valor_estoque >= valor_indicado:
+            print(f"\nCod#{livro.codigo}")
+            print(f"Título/Editora: {livro.titulo}/{livro.editora}")
+            print(f"Categoria: {livro.area}")
+            print(f"Ano: {livro.ano}")
+            print(f"Valor: R$ {livro.valor:.2f}")
+            print(f"Estoque: {livro.quantidade_estoque} unidades")
+            print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+    linha()
+
+# função busca livros por quantidade de unidade
+def numero_unidades():
+    quantidade = int(input("Digite o número de unidades desejada: "))
+    
+    for livro in lista_livros:
+        if livro.quantidade_estoque >= quantidade:
+            print(f"\nCod#{livro.codigo}")
+            print(f"Título/Editora: {livro.titulo}/{livro.editora}")
+            print(f"Categoria: {livro.area}")
+            print(f"Ano: {livro.ano}")
+            print(f"Valor: R$ {livro.valor:.2f}")
+            print(f"Estoque: {livro.quantidade_estoque} unidades")
+            
+            # chamando função para calcular o valor do Estoque
+            valor_estoque = calcula_valor_estoque(livro.valor,livro.quantidade_estoque)
+            print(f"Valor total em estoque: R$ {valor_estoque:.2f}")
+
+# adicionando livros na lista
+livro1 = lista_livros.append(Livro(titulo="O Avesso Da Pele",
+                            codigo="0301",
+                            editora="Companhia Das Letras",
+                            area="Romance",
+                            ano="2019",
+                            valor=47.60,
+                            quantidade_estoque=50))
+livro2 = lista_livros.append(Livro(titulo="Engenharia de Software",
+                            codigo="1203",
+                            editora="Pressman",
+                            area="Computação",
+                            ano="2011",
+                            valor=78,
+                            quantidade_estoque=100))
+
+# mensagem introdutória
+print("---- SISTEMA LIVRARIA ---- ")
+
+# menu principal do usuário
+def menu():
+    print("Essas são as opções do programa:\n")
+    print("1 – Cadastrar novo livro")
+    print('2 – Listar livros')
+    print('3 – Buscar livros por nome')
+    print('4 – Buscar livros por categoria')
+    print('5 – Buscar livros por preço')
+    print('6 – Busca por quantidade em estoque')
+    print('7 – Valor total no estoque')
+    print('0 – Encerrar atividades')
+    
+    print("\nO que deseja fazer?")
+    escolha = int(input("Sua escolha: "))
+
+
+
+
+
+
